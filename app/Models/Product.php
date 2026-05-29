8bf124aa4e16a4d13e8642cb8541554ae99f67f2
syabil038 <north2129@gmail.com>
2026-05-29 17:08:57 +0800

    install laravel dan crud category

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'name',
+        'price',
+        'stock',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
